5f42ba6bf5ded42c5860e8137dab3ebc44fe6345
realonereal <erohinreal@gmail.com>
2021-02-26 08:59:36 +0300

    fix: add finish code to index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];
+  let Array = str.split("");
+  let Bracket = 0;
+
+  while(Bracket < Array.length){
+  stack.push(Array[Bracket]);
+  for (let i = 0; i < bracketsConfig.length; i++){
+   if (stack[stack.length-2] == bracketsConfig[i][0] && stack[stack.length-1] == bracketsConfig[i][1]){
+    stack.pop();
+    stack.pop();
+   }
+  }
+    Bracket++;
+  }
+  return stack.length ? false : true;
 }
